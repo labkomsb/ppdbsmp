d35b1b4500e9064960481bad61f44d37e7ee6431
goparklubanic <nugroho.redbuff@gmail.com>
2019-09-03 15:28:08 +0700

    config dasar

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
   // variabel untuk home base / root directory
-  DEFINE ('BASEURL','http://localhost/DirectoryApp');
+  DEFINE ('BASEURL','http://localhost/ppdbsmp1/public/');
 
   // nama host database
   DEFINE ('dbhost', 'localhost');
   // nama database
-  DEFINE ('dbname', "namaBasisData");
+  DEFINE ('dbname', "ppdb");
   // namaUserDatabase
-  DEFINE ('dbuser', "userBasisData");
+  DEFINE ('dbuser', "userppdb");
   // passwordDatabase
   // disarankan : password terdiri dari huruf kecil, huruf kapital, angka dan tanda baca
-  DEFINE ('dbpass', "p@55Word");
+  DEFINE ('dbpass', "ppdb2020");
