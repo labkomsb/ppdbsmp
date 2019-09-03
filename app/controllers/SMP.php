01f89b4292100998dfa4e1927f3d1c6d8d93249d
ranipuspitasari <ranranipuspitasari@yahoo.com>
2019-09-03 16:27:37 +0700

    smp controller view

---
+++ app/controllers/SMP.php
@@ -0,0 +1,25 @@
+<?php
+// sesuaikan nama kelas, tetap extends ke Controller
+class SMP extends Controller
+{
+  // method default
+  public function index()
+  {
+      $this->view('smp/index');
+    // membambil dara dari model Model
+    /*
+    $data['xxx'] = $this->model('Model_xxx')->method();
+    // variabel untuk title bar
+    $data['title']="Home Page";
+
+    //
+    $this->view('template/header',$data);
+    $this->view('template/navbar');
+    // mengirim $data ke body
+    $this->view('home/index',$data);
+
+    // bodh halaman
+    $this->view('template/footer');
+    */
+  }
+}
