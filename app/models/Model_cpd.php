7f5014d64b67a8fd3bdc0804a948e42b969184e2
shinta098 <shintapus2@gmail.com>
2019-09-11 15:28:06 +0700

    update model

--- app/models/Model_cpd.php
+++ app/models/Model_cpd.php
@@ -40,12 +40,60 @@ class Model_template
     
     // update calon peserta didik
     public function cpdUpdate($data){
-    
+         // tulis perintah UPDATE
+         $sql = "UPDATE cpd SET nama = :nama ,	sandi = :md5sandi	, jkel = :jnkel	, tplahir	= :tplahir , tglahir = :tglahir ,	sd = :npsnsd	, smp = :npsnsmp ,	nbin = :nbin ,	nmat = :nmat ,	nipa	= :nipa , alamat = :alamat , WHERE nisn = :nisn";
+
+         // buat query
+         $this->db->query($sql);
+ 
+         // binding data
+         $this->db->bind('nisn' , $data['nisn']);
+         $this->db->bind('nama' , $data['nama']);
+         $this->db->bind('md5sandi' , $data['']);
+         $this->db->bind('jnkel' , $data['jnKel']);
+         $this->db->bind('tplahir' , $data['tplahir']);
+         $this->db->bind('tglahir' , $data['tglahir']);
+         $this->db->bind('npsnsd' , $data['sd']);
+         $this->db->bind('npsnsmp' , $data['smp']);
+         $this->db->bind('nbin' , $data['nbin']);
+         $this->db->bind('nmat' , $data['nmat']);
+         $this->db->bind('nipa' , $data['nipa']);
+         $this->db->bind('alamat' , $data['alamat']);
+ 
+         //eksekusi
+         $this->db->execute();
+ 
+         //laporkan hasil penyimpanan
+         return $this->db->rowCount();
     }
     
     // hapus calon peserta didik
     public function cpdHapus($data){
-        
+     // tulis perintah insert
+     $sql = "DELETE FROM cpd set nama = :nama ,	sandi = :md5sandi	, jkel = :jnkel	, tplahir	= :tplahir , tglahir = :tglahir ,	sd = :npsnsd	, smp = :npsnsmp ,	nbin = :nbin ,	nmat = :nmat ,	nipa	= :nipa , alamat = :alamat ,WHERE nisn = :nisn";
+
+     // buat query
+     $this->db->query($sql);
+
+     // binding data
+     $this->db->bind('nisn' , $data['nisn']);
+     $this->db->bind('nama' , $data['nama']);
+     $this->db->bind('md5sandi' , $data['']);
+     $this->db->bind('jnkel' , $data['jnKel']);
+     $this->db->bind('tplahir' , $data['tplahir']);
+     $this->db->bind('tglahir' , $data['tglahir']);
+     $this->db->bind('npsnsd' , $data['sd']);
+     $this->db->bind('npsnsmp' , $data['smp']);
+     $this->db->bind('nbin' , $data['nbin']);
+     $this->db->bind('nmat' , $data['nmat']);
+     $this->db->bind('nipa' , $data['nipa']);
+     $this->db->bind('alamat' , $data['alamat']);
+
+     //eksekusi
+     $this->db->execute();
+
+     //laporkan hasil penyimpanan
+     return $this->db->rowCount();    
     }
     
     // menampilkan detil calon peserta didik
