855391eca11319d847594a2eff4cbc64956d3785
goparklubanic <nugroho.redbuff@gmail.com>
2019-09-18 13:54:41 +0700

    model smp

---
+++ app/models/Model_smp.php
@@ -0,0 +1,33 @@
+<?php
+class Model_smp
+{
+    private $table = "cpd";
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function dataCPD(){
+        // perintah menampilkan data (npsn,nama,jkel,tplahir,tglahir,sd)
+        $sql = "";
+
+        // bungkus perintah
+        $this->db->query($sql);
+
+        // binding bila perlu
+
+
+        // eksekusi
+        $this->db->execute();
+
+        // tampung hasil pembacaan ke dalam variabel output
+        $output = $this->db->fetch();
+
+        // kembalikan output
+        return $output;
+
+    }
+
+}
